0bc47ef0ce5c31847df799682cd0912b43f65d6d
Joshua Rubin <jrubin@zvelo.com>
2017-05-29 21:27:33 -0600

    fix pager issues

--- startup/01_pager.zsh
+++ startup/01_pager.zsh
@@ -3,11 +3,15 @@ if [ -d ${HOME}/.vim/plugged/vimpager ]; then
   manpath[1,0]=${HOME}/.vim/plugged/vimpager
 fi
 
-if (( $#commands[nvim] )); then
-  export MANPAGER="nvim -c 'set ft=man' -"
-fi
-
 if (( $#commands[vimpager] )); then
   export PAGER=vimpager
   export MANPAGER=vimpager
+
+  # vimpager is broken on neovim which may be picked up from $EDITOR,
+  # force it to use regular vim
+  export VIMPAGER_VIM=vim
 fi
+
+# if (( $#commands[nvim] )); then
+#   export MANPAGER="nvim -c 'set ft=man' -"
+# fi
