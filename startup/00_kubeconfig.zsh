939567b7c59837f5eec522e9e0e3e86c070c812a
Joshua Rubin <jrubin@zvelo.com>
2017-06-02 12:04:29 -0600

    remove kubeconfig stuff
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- startup/00_kubeconfig.zsh
+++
@@ -1,9 +0,0 @@
-typeset -UTx KUBECONFIG kubeconfig
-
-if [[ -s $HOME/.kube/config ]]; then
-  kubeconfig+=$HOME/.kube/config
-fi
-
-if [[ -x $HOME/.kube/config.d ]]; then
-  kubeconfig+=($HOME/.kube/config.d/**/*)
-fi
