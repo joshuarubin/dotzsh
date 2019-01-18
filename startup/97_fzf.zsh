05f120beb2a28004b791b06efaa56f217eb7e3b0
Joshua Rubin <joshua@rubixconsulting.com>
2019-01-18 03:08:23 -0700

    add fzf config
    Signed-off-by: Joshua Rubin <joshua@rubixconsulting.com>

--- startup/97_fzf.zsh
+++ startup/97_fzf.zsh
@@ -1,5 +1,18 @@
 if (( $#commands[fzf] )); then
 
+if (( $#commands[rg] )); then
+  export FZF_DEFAULT_COMMAND='rg --files'
+  export FZF_CTRL_T_COMMAND=${FZF_DEFAULT_COMMAND}
+fi
+
+export FZF_DEFAULT_OPTS='
+  --inline-info
+  --ansi
+  --color fg:-1,bg:-1,hl:67,fg+:110,bg+:-1,hl+:67
+  --color info:229,prompt:242,pointer:73,marker:131,spinner:240
+'
+export FZF_CTRL_R_OPTS='--exact'
+
 # Key bindings
 # ------------
 if [[ $- == *i* ]]; then
